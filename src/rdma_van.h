38a245fce67305c56d63255b3cab726585792462
jiangyimin <jiangyimin@bytedance.com>
2019-12-13 11:17:48 +0800

    nit

--- src/rdma_van.h
+++ src/rdma_van.h
@@ -1,9 +1,18 @@
-/**
- *  Copyright (c) 2018-2019 Bytedance Inc.
- *  Author: lanchang@bytedance.com (Chang Lan)
- *          jiangyimin@bytedance.com (Yimin Jiang)
- *          chenjingrong@bytedance.com (Jingrong Chen)
-*/
+// Copyright 2019 Bytedance Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// =============================================================================
+
 #ifndef PS_RDMA_VAN_H_
 #define PS_RDMA_VAN_H_
 

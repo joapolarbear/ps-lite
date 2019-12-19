dacde24a81bc42abf03df39f4519fb91d805e0c6
jiangyimin <jiangyimin@bytedance.com>
2019-12-19 20:56:28 +0800

    split testcases into two files

--- tests/test_ipc_benchmark.cc
+++ tests/test_ipc_benchmark.cc
@@ -2,6 +2,12 @@
 #include <cmath>
 #include <cstdlib>
 #include <unistd.h>
+#include <fcntl.h>
+#include <numa.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/mman.h>
 #include "ps/ps.h"
 
 #define DIVUP(x, y) (((x)+(y)-1)/(y))
@@ -9,13 +15,38 @@
 
 using namespace ps;
 
-enum MODE {
-    PUSH_THEN_PULL = 0,
-    PUSH_PULL = 1,
-    PUSH_ONLY = 2, 
-    PULL_ONLY = 3
-};
-std::unordered_map<uint64_t, KVPairs<char> > mem_map;
+enum MODE { IPC };
+
+std::unordered_map<std::string, void *> _key_shm_addr;
+std::unordered_map<std::string, size_t> _key_shm_size;
+std::unordered_map<uint64_t, char*> store_;
+std::mutex mu_;
+
+void* OpenSharedMemory(const std::string& prefix, uint64_t key, size_t size) {
+  std::string shm_name(prefix);
+  shm_name += std::to_string(key);
+  int shm_fd = shm_open(shm_name.c_str(), O_CREAT | O_RDWR, 0666);
+  CHECK_GE(shm_fd, 0) << "shm_open failed for " << shm_name;
+  CHECK_GE(ftruncate(shm_fd, size), 0) << strerror(errno);
+
+  void* ptr = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+  CHECK_NE(ptr, (void*)-1) << strerror(errno);
+
+  LOG(INFO) << "initialized share memory size=" << size 
+            << " for key=" << key << ", name=" << shm_name;
+  _key_shm_addr[shm_name] = ptr;
+  _key_shm_size[shm_name] = size;
+  return ptr;
+}
+
+uint64_t EncodeKey(uint64_t seed) {
+  return seed << 16;
+}
+
+uint64_t DecodeKey(ps::Key key) {
+  auto kr = ps::Postoffice::Get()->GetServerKeyRanges()[ps::MyRank()];
+  return key - kr.begin();
+}
 
 void aligned_memory_alloc(void** ptr, size_t size) {
   size_t page_size = sysconf(_SC_PAGESIZE);
@@ -28,9 +59,10 @@ void aligned_memory_alloc(void** ptr, size_t size) {
   *ptr = p;
 }
 
+std::unordered_map<uint64_t, KVPairs<char> > mem_map;
 template <typename Val>
 void EmptyHandler(const KVMeta &req_meta, const KVPairs<Val> &req_data, KVServer<Val> *server) {
-  uint64_t key = req_data.keys[0];
+  uint64_t key = DecodeKey(req_data.keys[0]);
   if (req_meta.push) {
     CHECK(req_data.lens.size());
     CHECK_EQ(req_data.vals.size(), (size_t)req_data.lens[0]) 
@@ -78,53 +110,22 @@ void push_pull(KVWorker<char> &kv,
                std::vector<SArray<int> > &server_lens,
                int len, int num_servers, int total_key_num, 
                int how_many_key_per_server, MODE mode) {
-  CHECK_GT(mode, 0);
-  switch (mode) {
-    case PUSH_PULL: 
-      LOG(INFO) << "========= PUSH_PULL mode =========";
-      LOG(INFO) << "========= msg_size=" << len*sizeof(char) << " bytes =========";
-      break;
-    case PUSH_ONLY: 
-      LOG(INFO) << "========= PUSH_ONLY mode =========";
-      LOG(INFO) << "========= msg_size=" << len*sizeof(char) << " bytes =========";
-       break;
-    case PULL_ONLY: 
-      LOG(INFO) << "========= PULL_ONLY mode =========";
-      LOG(INFO) << "========= msg_size=" << len*sizeof(char) << " bytes =========";
-      break;
-    default: CHECK(0);
-  }
-
   std::vector<int> timestamp_list;
   auto start = std::chrono::high_resolution_clock::now();
   auto end = std::chrono::high_resolution_clock::now();
-  
   auto val = Environment::Get()->find("LOG_DURATION");
   unsigned int log_duration = val ? atoi(val) : 10;
   
   int cnt = 0;
   while (1) {
-    for (int key = 0; key < total_key_num; key++) {
-      auto keys = server_keys[key];
-      auto lens = server_lens[key];
-      auto vals = server_vals[key];
+    for (int i = 0; i < total_key_num; i++) {
+      auto key = EncodeKey(i);
+      auto keys = server_keys[i];
+      auto lens = server_lens[i];
+      auto vals = server_vals[i];
 
-      switch (mode) {
-        case PUSH_PULL: {
-          timestamp_list.push_back(kv.ZPush(keys, vals, lens));
-          timestamp_list.push_back(kv.ZPull(keys, &vals, &lens));
-        } break;
-        case PUSH_ONLY: {
-          timestamp_list.push_back(kv.ZPush(keys, vals, lens));
-        } break;
-        case PULL_ONLY: {
-          timestamp_list.push_back(kv.ZPull(keys, &vals, &lens));
-        } break;
-        default: {
-          CHECK(0);
-          break;
-        } 
-      }
+      timestamp_list.push_back(kv.ZPush(keys, vals, lens));
+      timestamp_list.push_back(kv.ZPull(keys, &vals, &lens));
     }
 
     for (auto& ts : timestamp_list) { kv.Wait(ts); }
@@ -153,30 +154,39 @@ void RunWorker(int argc, char *argv[]) {
 
   // init
   int len = (argc > 1) ? atoi(argv[1]) : 1024000;
-  int repeat = (argc > 2) ? atoi(argv[2]) : 10;
-  MODE mode = (argc > 3) ? static_cast<MODE>(atoi(argv[3])) : PUSH_PULL;
+  MODE mode = IPC;
+
+  size_t partition_bytes = Environment::Get()->find("BYTEPS_PARTITION_BYTES") ? 
+      atoi(Environment::Get()->find("BYTEPS_PARTITION_BYTES")) : 4096000;
+  CHECK_GE(partition_bytes, len) 
+      << "tensor partition is not supported in this benchmark"
+      << ", try reduce tensor size or increase BYTEPS_PARTITION_BYTES";
 
   auto v = Environment::Get()->find("NUM_KEY_PER_SERVER");
-  const int how_many_key_per_server = v ? atoi(v) : 40;
+  const int how_many_key_per_server = v ? atoi(v) : 20;
   const int total_key_num = num_servers * how_many_key_per_server;
 
   std::vector<SArray<char> > server_vals;
   std::vector<SArray<Key> > server_keys;
   std::vector<SArray<int> > server_lens;
-  for (int key = 0; key < total_key_num; key++) {
+  for (int i = 0; i < total_key_num; i++) {
+    auto key = EncodeKey(i);
     void* ptr;
-    aligned_memory_alloc(&ptr, len);
     SArray<char> vals;
-    vals.reset((char*) ptr, len * sizeof(char), [](void *){});
+    auto addr = (char*) OpenSharedMemory(std::string("BytePS_ShM_"), key, len);
+    vals.reset((char*) addr, len, [](void *){});
     server_vals.push_back(vals);
   }
 
   // init push, do not count this into time cost
-  for (int key = 0; key < total_key_num; key++) {
-    int server = key % num_servers;
-    PS_VLOG(1) << "key=" << key << " assigned to server " << server;
+  for (int i = 0; i < total_key_num; i++) {
+    int server = i % num_servers;
+    auto vals = server_vals[i];
 
-    auto vals = server_vals[key];
+    auto key = EncodeKey(i);
+    PS_VLOG(1) << "key=" << key
+               << " (i=" << i << ")" 
+               << " assigned to server " << server;
 
     // page aligned keys
     void* ptr_key;
@@ -198,65 +208,13 @@ void RunWorker(int argc, char *argv[]) {
     kv.Wait(kv.ZPush(keys, vals, lens));
   }
 
-  switch(mode) {
-    case PUSH_THEN_PULL: {
-      LOG(INFO) << "PUSH_THEN_PULL mode";
-      // push
-      uint64_t accumulated_ms = 0;
-      for (int i = 0; i < repeat; ++i) {
-        auto start = std::chrono::high_resolution_clock::now();
-        for (int server = 0; server < num_servers; server++) {
-          int key = server;
-          auto keys = server_keys[server];
-          auto lens = server_lens[server];
-          auto vals = server_vals[server];
-
-          kv.Wait(kv.ZPush(keys, vals, lens));
-        }
-        auto end = std::chrono::high_resolution_clock::now();
-        accumulated_ms += (end - start).count(); // ns
-      }
-      LL << "push " << len * sizeof(char)
-          << " bytes to each server, repeat=" << repeat
-          << ", total_time="
-          << accumulated_ms / 1e6 << "ms";
-
-      // pull
-      accumulated_ms = 0;
-      for (int i = 0; i < repeat; ++i) {
-        auto start = std::chrono::high_resolution_clock::now();
-        for (int server = 0; server < num_servers; server++) {
-          int key = server;
-          auto keys = server_keys[server];
-          auto lens = server_lens[server];
-          auto vals = server_vals[server];
-
-          kv.Wait(kv.ZPull(keys, &vals, &lens));
-        }
-        auto end = std::chrono::high_resolution_clock::now();
-        accumulated_ms += (end - start).count(); // ns
-      }
-
-      LL << "pull " << len * sizeof(char)
-          << " bytes to each server, repeat=" << repeat
-          << ", total_time="
-          << accumulated_ms / 1e6 << "ms";
-    } break;
-    case PUSH_PULL: 
-    case PUSH_ONLY: 
-    case PULL_ONLY: 
-      push_pull(kv, server_keys, server_vals, server_lens, len, num_servers, total_key_num, how_many_key_per_server, mode);
-      break;
-    default:
-      CHECK(0) << "unknown mode " << mode;
-  }
-
-
+  push_pull(kv, server_keys, server_vals, server_lens, len, num_servers, total_key_num, how_many_key_per_server, mode);
 }
 
 int main(int argc, char *argv[]) {
   // disable multi-threaded processing first
-  setenv("ENABLE_SERVER_MULTIPULL", "0", 1);
+  setenv("BYTEPS_LOCAL_SIZE", "1", 1);
+  setenv("BYTEPS_ENABLE_IPC", "1", 1);
   // start system
   Start(0);
   // setup server nodes
@@ -265,5 +223,10 @@ int main(int argc, char *argv[]) {
   RunWorker(argc, argv);
   // stop system
   Finalize(0, true);
+  // release shm
+  for (auto &it : _key_shm_addr) {
+    munmap(it.second, _key_shm_size[it.first]);
+    shm_unlink(it.first.c_str());
+  }
   return 0;
 }

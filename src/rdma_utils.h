1752620af8e49524433e2002e7581f0a69386b38
Yibo Zhu <zhuyibo@bytedance.com>
2019-12-20 09:36:37 -0800

    Allocate a whole page for meta

--- src/rdma_utils.h
+++ src/rdma_utils.h
@@ -71,8 +71,8 @@ static const int kMaxDataFields = 4;
 static const int kMaxResolveRetry = 50000;
 static const int kBasePort = 9010;
 
-// allocate 2KB more for meta with potentially variable length
-static const int kMaxMetaBound = 2048;
+// allocate a whole page for meta with potentially variable length
+static const int kMaxMetaBound = sysconf(_SC_PAGESIZE);
 
 // should have the same prefix with BytePS shared memory
 static const std::string kShmPrefix("BytePS_ShM_");
